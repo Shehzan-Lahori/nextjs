91d8a49a722f03a5a5972206aa7b3504572b6cc0
Shehzan-Lahori <lahorishehzan3@gmail.com>
2024-02-04 12:47:36 +0530

    finally done with middleware

--- carsales/convex/auth.config.js
+++ carsales/convex/auth.config.js
@@ -1,8 +1,8 @@
 export default {
-    providers: [
-      {
-        domain: "https://decent-griffon-90.clerk.accounts.dev",
-        applicationID: "convex",
-      },
-    ]
-  };
+  providers: [
+    {
+      domain: "https://decent-griffon-90.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
--- carsales/providers/convex-client-provider.tsx
+++ carsales/providers/convex-client-provider.tsx
@@ -3,21 +3,23 @@
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
-
-interface ConvexClientProviderProps{
-    children : React.ReactNode;
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
 }
 
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
-
 const convex = new ConvexReactClient(convexUrl);
 
-export const ConvexClientProvider = ({children} : ConvexClientProviderProps) => {
-    return(
-        <ClerkProvider>
-            <ConvexProviderWithClerk useAuth={useAuth} client = {convex}>
-            {children}
-            </ConvexProviderWithClerk>
-        </ClerkProvider>
-    );
+export const ConvexClientProvider = ({
+  children,
+}: ConvexClientProviderProps) => {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 };
